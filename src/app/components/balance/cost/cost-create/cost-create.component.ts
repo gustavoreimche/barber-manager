b1e5a6a0462210e4b79bfe7203d543ae43b506a7
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-23 13:30:09 -0300

    create components of cost

---
+++ src/app/components/balance/cost/cost-create/cost-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cost-create',
+  templateUrl: './cost-create.component.html',
+  styleUrls: ['./cost-create.component.scss']
+})
+export class CostCreateComponent {
+
+}
---
+++ src/app/components/balance/cost/cost-read/cost-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cost-read',
+  templateUrl: './cost-read.component.html',
+  styleUrls: ['./cost-read.component.scss']
+})
+export class CostReadComponent {
+
+}
