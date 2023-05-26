6b93ea413b9286ab00aa395fad072ef1b44dd26e
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-05-26 17:27:26 -0300

    adjustments app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,11 +75,11 @@ import { LoginComponent } from './components/login/login.component';
 import { CompanyFormComponent } from './components/company/company-form/company-form.component';
 import { ClientFormComponent } from './components/client/client-form/client-form.component';
 import { HomeComponent } from './components/home/home/home.component';
-import { EntryComponent } from './components/entry/entry/entry.component';
 import { EntryCreateComponent } from './components/balance/entry/entry-create/entry-create.component';
 import { EntryDeleteComponent } from './components/balance/entry/entry-delete/entry-delete.component';
 import { EntryReadComponent } from './components/balance/entry/entry-read/entry-read.component';
 import { EntryUpdateComponent } from './components/balance/entry/entry-update/entry-update.component';
+import { EntryComponent } from './components/balance/entry/entry/entry.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -117,6 +117,7 @@ registerLocaleData(localePt);
     EntryDeleteComponent,
     EntryReadComponent,
     EntryUpdateComponent,
+    BalanceComponent,
   ],
   imports: [
     BrowserModule,
