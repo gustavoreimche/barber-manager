8b0409ff91ddfd7c323f95b554615d4d92114be2
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-05-26 16:53:43 -0300

    card month select add

--- src/app/components/balance/balance-home/balance.component.ts
+++ src/app/components/balance/balance-home/balance.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
-  styleUrls: ['./balance.component.scss']
+  styleUrls: ['./balance.component.css']
 })
 export class BalanceComponent {
-
+  selectedMonth: string;
+  months: string[] = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  
+  constructor() {
+    this.selectedMonth = this.months[0]; 
+  }
 }
